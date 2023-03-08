1531f56b7b7649e0b2feb30eeb1fabf022ec398b
Omar Rwemi <omsharp@gmail.com>
2023-03-08 17:52:59 +0200

    prettier setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 // import { invoke } from "@tauri-apps/api/tauri"
-import type { NextPage } from "next"
+import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
     <div>
+      dd
       <h1>Welcome to Tauri with NextJS</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
